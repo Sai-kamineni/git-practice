94c760920dc7bb7d568a5b7b7f100e0715581bad
Sai-kamineni <saikumar.dba10@gmail.com>
2024-11-14 17:57:55 +0530

    functions

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 
+##Checking user is root or not
+
 USERID=$(id -u)
 #echo "User ID is: $USERID"
 
@@ -10,4 +12,22 @@ then
     exit 1
 fi
 
-dnf list installed git
+
+###Installing git
+
+dnf list installed git   #checking wheter installed or not
+
+if [ $? -ne 0 ]
+then 
+    echo "Git is not installed, installing the git"
+    dnf install git -y  
+    if [ $? -ne 0 ]
+    then 
+        echo "Git installation not completed, please check"
+        exist 1   #script exist here
+    else    
+        echo "Git installation completed"
+    fi
+else 
+    echo "Git already installed"
+fi
