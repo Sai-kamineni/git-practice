9dd137cc4770b5e7dd92bc027cf5aefe35b0cfe3
Sai-kamineni <saikumar.dba10@gmail.com>
2024-11-15 16:39:53 +0530

    functions

---
+++ redirector_usage.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+LOG_FOLER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)  #print in format 2024-11-15-10-45-55
+LOG_FILE="$LOG_FOLER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOG_FOLER   #creates folder if not created
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root priveleges $N" &>>$LOG_FILE
+        exit 1
+    fi
+}
+
+USAGE(){
+    echo -e "$R USAGE :: $N provide input arguments.."
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N" &>>$LOG_FILE
+    fi
+}
+
+CHECK_ROOT
+
+if [ $# -eq 0 ]
+then
+    USAGE
+fi
+
+for package in $@
+do
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ]
+    then
+        echo "$package is not installed, installing it.." &>>$LOG_FILE
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "Installing $package" 
+    else
+        echo -e "$package is already $Y installed $N" &>>$LOG_FILE
+    fi   
+done
